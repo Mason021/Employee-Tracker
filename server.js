0cc19182a4c7499447a2213f462c1bdf8e4c865e
Mason021 <mdc5054@gmail.com>
2021-08-19 11:25:50 -0500

    removed important information for myself

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Dampy0231@#%',
+    password: '',
     database: 'employee_DB'
 });
 
